b313c86c494d24dc20f963d3d19e2c7d9726d475
VladisS-Vostok2000 <VladisS.Vostok2000@gmail.com>
2021-08-27 16:42:45 +0300

    ConsoleVoidControl maintain

---
+++ ConsoleDrawingEngine/Types/Pictures/ConsoleSingleCharPicture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static Game.ExtensionMethods.BasicTypesExtensionsMethods;
+using static Game.ExtensionMethods.ConsoleExtensionMethods;
+using static Game.ConsoleDrawingEngine.ConsoleDrawing;
+using Game.ColoredCharsEngine.Pictures;
+
+namespace Game.ConsoleDrawingEngine.Types.Pictures {
+    public class ConsoleSingleCharPicture : ConsolePicture {
+
+
+
+        public ConsoleSingleCharPicture(SingleCharPicture picture) : base(picture) {
+
+        }
+
+
+
+        public override void VisualizeInConsole(Point location) {
+            SingleCharPicture picture = Picture as SingleCharPicture;
+            string str = new string(picture.Char, picture.Width);
+            CursorTop = location.Y;
+            for (int i = 0; i < picture.Height; i++) {
+                CursorLeft = location.X;
+                CursorTop += i;
+                ConsoleDrawing.Write(str);
+            }
+        }
+    }
+}
